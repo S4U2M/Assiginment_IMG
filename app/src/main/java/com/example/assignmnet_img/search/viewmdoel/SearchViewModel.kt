66b4e5cca6c507806e4eb6cf1f3dc60139cf2675
송규진 <songu9610@gmail.com>
2023-09-19 16:13:50 +0900

    [FEAT] : ⚙️: 리스트를 추가하는 메소드 구현
    리스트를 추가함

--- app/src/main/java/com/example/assignmnet_img/search/viewmdoel/SearchViewModel.kt
+++ app/src/main/java/com/example/assignmnet_img/search/viewmdoel/SearchViewModel.kt
@@ -11,5 +11,24 @@ class SearchViewModel: ViewModel() {
     private val _searchList : MutableLiveData<List<SearchModel>> = MutableLiveData()
     val searchList : LiveData<List<SearchModel>> get() = _searchList
 
+    init {
+        _searchList.value = mutableListOf<SearchModel>().apply {
+            for(i in 0 .. 3){
+                add(
+                    SearchModel(
+                        id = i+10000.toLong(),
+                        imageUrl = "qr",
+                        displaySiteName = "11",
+                        datetime = "$i"
+                    )
+                )
+            }
+        }
+    }
 
+    fun getList(list:List<SearchModel>){
+
+        _searchList.value = list
+
+    }
 }
