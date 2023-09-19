cb8ff15ac25ae09fa86ab5567c586e788251b8b7
송규진 <songu9610@gmail.com>
2023-09-19 16:13:27 +0900

    [FEAT] : ⚙️: 아이디 부여
    아이디 부여함

--- app/src/main/java/com/example/assignmnet_img/search/dataclass/SearchModel.kt
+++ app/src/main/java/com/example/assignmnet_img/search/dataclass/SearchModel.kt
@@ -2,6 +2,7 @@ package com.example.assignmnet_img.search.dataclass
 
 data class SearchModel
     (
+    val id : Long = -1,
     val imageUrl: String,
     val displaySiteName: String,
     val datetime: String
