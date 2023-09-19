b8f5e5b62ad5a7ded30be05d5817d24e86a12de5
송규진 <songu9610@gmail.com>
2023-09-19 20:23:30 +0900

    [FEAT] : ⚙️: 함수 추가
    북마크 모델로 변경시켜주는 함수 추가

--- app/src/main/java/com/example/assignmnet_img/search/dataclass/SearchModel.kt
+++ app/src/main/java/com/example/assignmnet_img/search/dataclass/SearchModel.kt
@@ -1,9 +1,20 @@
 package com.example.assignmnet_img.search.dataclass
 
+import com.example.assignmnet_img.bookmark.BookmarkModel
+
 data class SearchModel
     (
-    val id : Long = -1,
+    val id: Long = -1,
     val imageUrl: String,
     val displaySiteName: String,
     val datetime: String
 )
+
+fun SearchModel.toBookmarkModel(): BookmarkModel {
+    return BookmarkModel(
+        id = id,
+        imageUrl = imageUrl,
+        displaySiteName = displaySiteName,
+        datetime = datetime
+    )
+}
