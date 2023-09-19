a25cad0deeaaab9782fcb628fc6249437583e300
송규진 <songu9610@gmail.com>
2023-09-19 10:22:59 +0900

    [FIX] : 🔧: 버그수정
    okhttp추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,9 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    //okhttp
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+
     //hash key를 받기 위함
     implementation ("com.kakao.sdk:v2-all:2.16.0")
 
