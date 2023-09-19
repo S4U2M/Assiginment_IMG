13801d4962aa68667641aa62502f5c0d9870f066
송규진 <songu9610@gmail.com>
2023-09-19 18:36:06 +0900

    [FIX] : 🔧: sharedViewModel 사용을 위한 activity,fragment추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,9 @@ android {
 
 dependencies {
 
+    //glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
     //retrofit2
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
@@ -69,14 +72,16 @@ dependencies {
     val lifecycle_version = "2.5.1"
     val arch_version = "2.1.0"
 
-    //glide
-    implementation("com.github.bumptech.glide:glide:4.16.0")
+    //activityViewmodel
+    implementation("androidx.activity:activity-ktx:1.5.7")
+    implementation("androidx.fragment:fragment-ktx:1.5.7")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycle_version")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
 
     implementation("androidx.core:core-ktx:1.9.0")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
