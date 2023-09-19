53e5e9861f576490d36ec852d418bedfaa679837
송규진 <songu9610@gmail.com>
2023-09-19 20:21:01 +0900

    [FEAT] : ⚙️: 북마크 모델 설계

--- app/src/main/java/com/example/assignmnet_img/bookmark/BookMarkViewModel.kt
+++ app/src/main/java/com/example/assignmnet_img/bookmark/BookMarkViewModel.kt
@@ -1,8 +1,22 @@
 package com.example.assignmnet_img.bookmark
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class BookMarkViewModel:ViewModel() {
+class BookMarkViewModel : ViewModel() {
 
+    private val _bookmarkList: MutableLiveData<List<BookmarkModel>> = MutableLiveData()
+    val bookmarkList: LiveData<List<BookmarkModel>> get() = _bookmarkList
 
+    fun addModel(item: BookmarkModel?) {
+        if (item == null) return
+
+        val currentList = bookmarkList.value.orEmpty().toMutableList().apply {
+            add(item)
+        }
+
+        _bookmarkList.value = currentList
+
+    }
 }
