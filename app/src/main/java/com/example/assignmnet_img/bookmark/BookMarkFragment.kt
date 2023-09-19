5f8c32ddadd424a220c43d8af6a0e814062dc252
송규진 <songu9610@gmail.com>
2023-09-19 20:22:00 +0900

    [FEAT] : ⚙️: 연결
    어뎁터
    뷰모델
    sharedviewmodel

--- app/src/main/java/com/example/assignmnet_img/bookmark/BookMarkFragment.kt
+++ app/src/main/java/com/example/assignmnet_img/bookmark/BookMarkFragment.kt
@@ -5,38 +5,66 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
 import com.example.assignmnet_img.R
 import com.example.assignmnet_img.databinding.BookMarkFragmentBinding
-
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.assignmnet_img.main.SharedViewModel
+import com.example.assignmnet_img.search.dataclass.toBookmarkModel
 
 
 class BookMarkFragment : Fragment() {
 
-    private var _binding:BookMarkFragmentBinding? = null
-    private val binding get() =  _binding
+    companion object {
 
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
+
+
+    private var _binding: BookMarkFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    private val bookMarkAdapter by lazy {
+        BookMarkAdapter()
+    }
+
+    private val bookmarkViewModel by lazy {
+        ViewModelProvider(this)[BookMarkViewModel::class.java]
+    }
+
+    private val sharedViewModel: SharedViewModel by activityViewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.book_mark_fragment, container, false)
+    ): View {
+        _binding = BookMarkFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        initView()
+        initViewModel()
     }
+
+    private fun initView() = with(binding) {
+        bookmarkRcList.adapter = bookMarkAdapter
+    }
+
+    private fun initViewModel() = with(bookmarkViewModel) {
+        bookmarkList.observe(viewLifecycleOwner) {
+            bookMarkAdapter.submitList(it)
+        }
+
+        sharedViewModel.liveSearchModel.observe(viewLifecycleOwner) {
+            val updateBookmarkModel =
+                sharedViewModel.liveSearchModel.value?.toBookmarkModel() ?: return@observe
+
+            addModel(updateBookmarkModel)
+        }
+    }
+
+
 }
